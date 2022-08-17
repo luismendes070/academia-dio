7f17f3a80509b3f9ba30cb9142f4c9ca07f622fb
Luis Mendes <machadoluismendes@gmail.com>
2022-08-17 16:34:06 -0300

    DIO Spring Framework Experience Code Challenge
    Code Challange
    Spring Boot
    Spring MVC
    Spring Web
    lombok
    postgreSQL

---
+++ academia-dio/academia-dio/src/main/java/dio/innovation/academiadio/AcademiaDioApplication.java
@@ -0,0 +1,13 @@
+package dio.innovation.academiadio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaDioApplication.class, args);
+	}
+
+}
